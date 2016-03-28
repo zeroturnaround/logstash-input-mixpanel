7ae54bb38d7253b40b84c7c0848109a76923be1a
andrus7k <andrus@a7k.pri.ee>
2016-03-28 17:19:55 +0300

    added engage api

--- lib/logstash/inputs/mixpanel.rb
+++ lib/logstash/inputs/mixpanel.rb
@@ -16,51 +16,72 @@ class LogStash::Inputs::Mixpanel < LogStash::Inputs::Base
   # If undefined, Logstash will complain, even if codec is unused.
   default :codec, "plain"
 
+  #
+  config :engage, validate: :boolean, required: true
+
   # The API key of the project
   config :api_key, validate: :string, required: true
 
   # The Secret of the project
   config :api_secret, validate: :string, required: true
 
-  # Set how frequently messages should be sent.
-  #
-  # The default, `1`, means send a message every second.
-  config :schedule, validate: :string
-
   public
   def register
-    require "rufus/scheduler"
     @client = Mixpanel::Client.new(api_key: @api_key, api_secret: @api_secret)
-  end # def register
+  end
+
+  # def register
 
   def run(queue)
-    if @schedule
-      setup_scheduler(queue)
-    else
-      fetch(queue)
-    end
+    fetch(queue)
+  end
 
-  end # def run
+  # def run
 
   private
 
-  def setup_scheduler(queue)
-    @scheduler = Rufus::Scheduler.new
-    @scheduler.cron(@schedule) do
-      fetch(queue)
+  def fetch(queue)
+    if (@engage)
+      fetch_engage(queue)
+    else
+      fetch_export(queue)
     end
-    @scheduler.join
   end
 
-  def fetch(queue)
+  def fetch_export(queue)
     @client
-      .request("export", from_date: (Date.today - 1).to_s, to_date: (Date.today - 1).to_s)
-      .each do |raw_event|
-        event = LogStash::Event.new raw_event
-        event["@timestamp"] = LogStash::Timestamp.at(event["properties"]["time"])
-        decorate(event)
-        queue << event
-      end
+        .request("export", from_date: (Date.today - 1).to_s, to_date: (Date.today - 1).to_s)
+        .each do |raw_event|
+      event = LogStash::Event.new raw_event
+      event["@timestamp"] = LogStash::Timestamp.at(event["properties"]["time"])
+      decorate(event)
+      queue << event
+    end
+  end
+
+  def fetch_engage(queue)
+    this_page = fetch_engage_page()
+    queue_engaged_page(queue, this_page)
+
+    while (this_page and this_page['results'].size > 0)
+      this_page = fetch_engage_page(this_page['page'].to_i + 1, this_page['session_id'])
+      queue_engaged_page(queue, this_page)
+    end
+  end
+
+  def fetch_engage_page(page_nr = 0, session_id = nil)
+    data = nil
+    if (session_id)
+      data = @client.request("engage", page: page_nr, session_id: session_id)
+    else
+      data = @client.request("engage", page: page_nr)
+    end
+    data['results'].each do |raw_event|
+      event = LogStash::Event.new raw_event
+      decorate(event)
+      queue << event
+    end
+    data
   end
 
 end # class LogStash::Inputs::Example
--- logstash-input-mixpanel.gemspec
+++ logstash-input-mixpanel.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'logstash-input-mixpanel'
-  s.version         = '0.1.3'
+  s.version = '0.2.5'
   s.licenses = ['Apache License (2.0)']
   s.summary = "This example input streams a string at a definable interval."
   s.description = "This gem is a logstash plugin required to be installed on top of the Logstash core pipeline using $LS_HOME/bin/plugin install gemname. This gem is not a stand-alone program"
@@ -10,18 +10,26 @@ Gem::Specification.new do |s|
   s.require_paths = ["lib"]
 
   # Files
-  s.files = `git ls-files`.split($\)
-   # Tests
+  s.files = [".gitignore",
+             "DEVELOPER.md",
+             "Gemfile",
+             "LICENSE",
+             "README.md",
+             "Rakefile",
+             "lib/logstash/inputs/mixpanel.rb",
+             "logstash-input-mixpanel.gemspec",
+             "spec/inputs/mixpanel_spec.rb"]
+
+  # Tests
   s.test_files = s.files.grep(%r{^(test|spec|features)/})
 
   # Special flag to let us know this is actually a logstash plugin
-  s.metadata = { "logstash_plugin" => "true", "logstash_group" => "input" }
+  s.metadata = {"logstash_plugin" => "true", "logstash_group" => "input"}
 
   # Gem dependencies
-  s.add_runtime_dependency "logstash-core", '>= 1.4.0', '< 2.0.0'
+  s.add_runtime_dependency "logstash-core", '>= 1.4.0', '< 2.3.0'
   s.add_runtime_dependency 'logstash-codec-plain'
   s.add_runtime_dependency 'stud'
   s.add_runtime_dependency 'mixpanel_client'
-  s.add_runtime_dependency 'rufus-scheduler', '3.0.9'
   s.add_development_dependency 'logstash-devutils'
 end
